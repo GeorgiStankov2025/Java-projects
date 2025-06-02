b07564b97e0a2fde2778f1a82f462377c8468c9d
GeorgiStankov2025 <georgistankov05@gmail.com>
2025-06-02 20:50:10 +0300

    Update and rename BachkatoriMain.java to WorkersMain.java

--- 16.05.2025/src/WorkersMain.java
+++ 16.05.2025/src/WorkersMain.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class BachkatoriMain {
+public class WorkersMain {
     public static void main(String[] args)
     {
 
-        ArrayList<Bachkator> list =new ArrayList<Bachkator>();
+        ArrayList<Worker> list =new ArrayList<Worker>();
 
-        list.add(new DnevenBachkator("Georgi",100,100));
+        list.add(new DayWorker("George",100,100));
 
-        list.add(new NoshtenBachkator("Angel",150,90));
+        list.add(new NoshtenBachkator("Peter",150,90));
 
         for(int i=0;i<list.size();i++)
         {
